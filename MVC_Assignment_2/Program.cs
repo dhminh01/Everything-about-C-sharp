a0d02f0174b2f6c1822d516371460f7207be77eb
dhminh01 <duonghongminh1105@gmail.com>
2025-04-02 20:13:39 +0700

    MVC Assignment 2 final

---
+++ MVC_Assignment_2/Program.cs
@@ -0,0 +1,36 @@
+using MVC_Assignment_2.Interfaces;
+using MVC_Assignment_2.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<IPersonService, PersonService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+app.MapControllerRoute(
+    name: "NashTech",
+    pattern: "NashTech/{controller=Rookies}/{action=Index}/{id?}"
+).WithStaticAssets();
+
+app.Run();
